ce41de9b91ae5846b3901088f07dc08d48feff42
Victorien Schweitzer <victorien-s@live.fr>
2017-03-08 00:20:29 +0100

    Ajout d'un affichage des resultats simple, barre de progression

--- addon.py
+++ addon.py
@@ -1,25 +1,36 @@
 import sys
 import xbmc
 import xbmcgui
-import gui
 from lib import tools
 from lib import info
 from lib import github
 
 __addonname__       = info.__addonname__
 translate           = tools.translate
-
+dialog              = xbmcgui.Dialog()
+dialogProgress      = xbmcgui.DialogProgress()
 
 while True:
-    index = xbmcgui.Dialog().select(__addonname__, [translate(32101)])
+    index = dialog.select(__addonname__, [translate(32101)])
     # GitHub selected
     if index == 0:
         keyboard = xbmc.Keyboard('2sec', translate(32020))
         keyboard.doModal()
         if keyboard.isConfirmed():
             value_search = keyboard.getText()
+            dialogProgress.create(__addonname__,translate(32021),'...')
+            dialogProgress.update(20,translate(32021),'...')
             search_result = github.getSearchResult({ 'q' : value_search })
-            xbmcgui.Dialog().ok(__addonname__, search_result['items'][1]['downloads_url'])
+            dialogProgress.update(50,translate(32021),'...')
+            listResult=[]
+            for singleResult in search_result['items']:
+                if singleResult['full_name'] != None:
+                    listResult.append(singleResult['full_name'])
+            dialogProgress.update(100,translate(32021),'...')
+            dialogProgress.close()
+            select=dialog.select(translate(32022), listResult)
+            if select>=0:
+                selectedResult=search_result['items'][select]
         break
     else:
         break
